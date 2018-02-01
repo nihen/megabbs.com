b17ccf4248533990e9dc38d78ae5aaee375d7bff
Masahiro Chiba <nihen@megabbs.com>
2018-02-01 23:16:24 +0900

    import files

---
+++ app/404.py
@@ -0,0 +1,25 @@
+#!-*- coding:utf-8 -*-
+print 'Status: 404 Not Found'
+print 'Content-Type: text/html'
+print ''
+print """
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>404 megabbs Not Found</title>
+    </head>
+    <body>
+        <div id="oshimeri">
+            <img src="/static/maturiakita.jpg" />
+
+            <a href="/fin.html">おしめりです</a>
+            <span id="thanks to" style="display: none">
+                Thanks to ひろゆき 2ch 2chロビー 元服太郎 まみまみ ま～ mono タヒネズミ カブ3才 歴代削除人 ふくやん zenhitei 共同BBS企画 virtualave WebArena BIG-Server さくらインターネット
+            </span>
+        </div>
+	</body>
+</html>
+"""
+
